8e4a5af7f2a46dea1ab290f1b85ddc0052c325c0
Akousse <amer.axel@hotmi>
2018-04-01 23:31:11 +0200

    traductions messages

--- resources/lang/fr/message.php
+++ resources/lang/fr/message.php
@@ -9,10 +9,44 @@ return [
     */
 
     'awaiting_offers' => [
-        'title' => 'Offres en attente'
+        'title' => 'Offres en attente',
+        'confirm_denial_message' => 'Cette offre a bien été refusé.',
+        'confirm_accept' => 'Offre acceptée ! Commencer la discussion dès maintenant.'
     ],
 
-    'empty' => 'Vous n\'avez pas encore de messages!'
+
+    'empty' => 'Vous n\'avez pas encore de messages!',
+
+
+    'discussions' => [
+        'title' => 'Discussions'
+    ],
+
+    'success' => [
+        'sold' => 'Votre ticket a été vendu! Merci de votre confiance.'
+    ],
+
+    'errors' => [
+        'not_found' => 'Discussion introuvable.',
+        'ticket_not_found' => 'Ticket introuvable',
+        'cant_accept' => 'L\'offre ne peut être acceptée',
+        'not_active' => 'Cette conversation est inactive',
+        'wrong_user' => 'Cette conversation n\'est pas la votre!',
+        'wrong_ticket_discussion' => 'La combinaison ticket-discussion a échoué.',
+        'something' => 'Quelque chose n\'a pas fonctionné.',
+        'already_sold' => 'Ce ticket est déjà en vente.'
+    ],
+
+    'discussion_page' => [
+        'modals' => [
+            'sell' => [
+                'first_part' => 'Vous êtes sur le point de marqué ce ticket comme vendu.',
+                'secund_part' => 'Veuillez vous assurer de ne cliquer ici uniquement après avoir reçu le paiement de l\'acheteur.',
+                'third_part' => 'va automatiquement recevoir votre billet.'
+            ]
+        ]
+    ]
 
 
 ];
+
