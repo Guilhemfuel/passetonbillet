3d0ad7c4b7385d31bdb9d71b1d0317cacf138a22
JhumanJ <julien@nahum.net>
2018-04-06 19:49:01 +0100

    Completely finish v2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,6 +123,20 @@
     });
 </script>
 @stack('scripts')
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117087751-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-117087751-1');
+
+    @if(Auth::check())
+    gtag('set', {'user_id': {{Auth::user()->id}} }); // Set the user ID using signed-in user_id.
+    @endif
+</script>
+
 
 </body>
 </html>
