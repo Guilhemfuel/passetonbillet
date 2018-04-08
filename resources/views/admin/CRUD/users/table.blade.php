c98e21992cffe307994a8b67007b16a14d29c47c
JhumanJ <julien@nahum.net>
2018-04-08 18:14:28 +0100

    Add phone verified

--- resources/views/admin/CRUD/users/table.blade.php
+++ resources/views/admin/CRUD/users/table.blade.php
@@ -6,6 +6,7 @@
             <th>First Name</th>
             <th>Last Name</th>
             <th>Status</th>
+            <th class="text-center">Phone</th>
             <th class="text-center">Facebook</th>
             <th class="text-center">Verified</th>
             <th>Actions</th>
@@ -16,6 +17,13 @@
                     <td>{{$entity->first_name}}</td>
                     <td>{{$entity->last_name}}</td>
                     <td>{{$entity->role}}</td>
+                    <td class="text-center">
+                        @if($entity->phone_verified)
+                            <i class="fa fa-phone-square text-success" aria-hidden="true"></i>
+                        @else
+                            X
+                        @endif
+                    </td>
                     <td class="text-center">{!!$entity->fb_id!=null?'<a href="https://facebook.com/'.$entity->fb_id.'" target="_blank"><i aria-hidden="true" class="fa fa-facebook-official text-facebook"></i></a>':'X'!!}</td>
                     <td class="text-center">{!!$entity->id_verified?'<i aria-hidden="true" class="fa fa-check-circle text-warning"></i>':'X'!!}</td>
                     <td>
