8cd30fc43494a431069c2b5b2427bd3b37b67bdc
JhumanJ <julien@nahum.net>
2018-04-09 13:17:33 +0100

    Improve admin tickets table list

--- resources/views/admin/CRUD/tickets/table.blade.php
+++ resources/views/admin/CRUD/tickets/table.blade.php
@@ -5,22 +5,28 @@
         <thead>
         <th>Seller Email</th>
         <th>Date</th>
+        <th>From</th>
+        <th>To</th>
         <th>Price</th>
-        <th>Currency</th>
         <th>Actions</th>
         </thead>
         <tbody>
         @foreach($entities as $entity)
             <tr>
                 <td>
-                    <a href="{{route('users.edit',$entity->user->id)}}">{{$entity->user->email}}</a>
+                    <a href="{{route('users.edit',$entity->user->id)}}">{{$entity->user->full_name}}</a>
                     <span class="text-danger pull-right">{{$entity->eurostar_ticket_number==null?'FAKE':''}}</span>
                 </td>
                 <td>
-                    {{$entity->train->departure_date}}
+                    {{$entity->train->carbon_departure_date->format('d/m/Y')  }}
                 </td>
-                <td>{{$entity->price}}</td>
-                <td>{{$entity->currency}}</td>
+                <td>
+                    {{substr($entity->train->departureCity->short_name,2)}}
+                </td>
+                <td>
+                    {{ substr($entity->train->arrivalCity->short_name,2)  }}
+                </td>
+                <td>{{$entity->price}} {{$entity->currency}}</td>
                 <td>
                     <a class="btn btn-sm btn-info btn-fill" href="{{route('tickets.edit',$entity->id)}}">
                         <i class="fa fa-pencil" aria-hidden="true"></i>
