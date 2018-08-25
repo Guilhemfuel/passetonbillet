2119cf42da73f7ebc26ae4d61a0cbd18e09cb6bb
JhumanJ <julien@nahum.net>
2018-08-25 14:22:41 +0100

    Add flush cache command to regenerate lang files

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\FlushCache;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\UpdateTrains::class,
-        Commands\Empower::class
+        Commands\Empower::class,
+        FlushCache::class
     ];
 
     /**
--- webpack.mix.js
+++ webpack.mix.js
@@ -22,7 +22,8 @@ mix.js('resources/assets/js/app.js', 'public/js').version()
     .sass('resources/assets/sass/admin.scss', 'public/css').version()
     .copyDirectory('resources/assets/img', 'public/img')
     .copyDirectory('resources/assets/audio', 'public/audio')
-    .copyDirectory('resources/assets/fonts', 'public/fonts');
+    .copyDirectory('resources/assets/fonts', 'public/fonts')
+    .language();
 
 
 
