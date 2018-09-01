96779a7ec6b10c49eeaafc89f1d26be046be7b8e
JhumanJ <julien@nahum.net>
2018-09-01 19:48:09 +0200

    Working on station search - about to replace levenshtein with similar text

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -29,7 +29,20 @@ class StationController extends Controller
         }
 
         $name = \AppHelper::removeAccents( $request->name );
+        $name = strtolower( $name );
+        $secundName = null;
+
         $nameTrigram = ( new TNTIndexer() )->buildTrigrams( $name );
+        $secundNameTrigram = null;
+
+        // If name starts with 'saint' or 'st', search for the other one
+        if ( preg_match( "/^(saint)(\s|-)\w*/i", $name ) ) {
+            $secundName = preg_replace( "/^(saint)\w*/i", "st", $name );
+            $secundNameTrigram = ( new TNTIndexer() )->buildTrigrams( $secundName );
+        } elseif ( preg_match( "/^(st)(\s|-)\w*/i", $name ) ) {
+            $secundName = preg_replace( "/^(st)\w*/i", "saint", $name );
+            $secundNameTrigram = ( new TNTIndexer() )->buildTrigrams( $secundName );
+        }
 
         $tnt = new TNTSearch();
 
@@ -43,25 +56,72 @@ class StationController extends Controller
         $res = $tnt->search( $nameTrigram, 10 );
         $keys = collect( $res['ids'] )->values()->all();
 
+        // If secund name, also search for it
+        if ( $secundNameTrigram ) {
+            $res = $tnt->search( $secundNameTrigram, 10 );
+            $keys = array_merge( $keys, collect( $res['ids'] )->values()->all() );
+        }
+
         $stations = Station::whereIn( 'id', $keys )->get();
 
-        $stations->map( function ( $station ) use ( $name ) {
-            $station->distance = min(
-                levenshtein( $name, $station->name ),
-                levenshtein( $name, $station->name_fr ),
-                levenshtein( $name, $station->name_en )
-            );
+        $stations->map( function ( $station ) use ( $name, $secundName ) {
+
+            $stationName = \App\Facades\AppHelper::removeAccents($station->name);
+            $stationNameFR = $station->name_fr ? \App\Facades\AppHelper::removeAccents($station->name_fr) : null;
+            $stationNameEN = $station->name_en ? \App\Facades\AppHelper::removeAccents($station->name_en): null;
+
+            if ( $secundName ) {
+                $station->distance = min(
+                    levenshtein( $name, $stationName, 2, 1, 2 ),
+                    levenshtein( $name, $stationNameFR, 2, 1, 2 ),
+                    levenshtein( $name, $stationNameEN, 2, 1, 2 ),
+                    // test secund name distance
+                    levenshtein( $secundName, $stationName, 2, 1, 2 ),
+                    levenshtein( $secundName, $stationNameFR, 2, 1, 2 ),
+                    levenshtein( $secundName, $stationNameEN, 2, 1, 2 )
+                );
+            } else {
+                $station->distance = min(
+                    levenshtein( $name, $stationName, 2, 1, 2 ),
+                    levenshtein( $name, $stationNameFR, 2, 1, 2 ),
+                    levenshtein( $name, $stationNameEN, 2, 1, 2 )
+                );
+            }
         } );
 
-        $sorted = $stations->sort( function ( $a, $b ) {
+
+        $sorted = $stations->sort( function ( $a, $b ) use ( $name, $secundName ) {
+
+            $stationNameA = \App\Facades\AppHelper::removeAccents($a->name);
+            $stationNameB = \App\Facades\AppHelper::removeAccents($b->name);
+
             if ( $a->distance === $b->distance ) {
-                return 0;
+                // Priority to the one with the name closer (not help)
+                $distanceA = levenshtein( $stationNameA, $name, 2, 1, 2 );
+                $distanceB = levenshtein( $stationNameB, $name, 2, 1, 2 );
+
+                // Also consider secund name
+                if ($secundName) {
+                    $distanceA = min($distanceA,levenshtein( $stationNameA, $secundName, 2, 1, 2 ));
+                    $distanceB = min($distanceB,levenshtein( $stationNameB, $secundName, 2, 1, 2 ));
+                }
+
+                if ($distanceA === $distanceB) return 0;
+
+                return $distanceA < $distanceB ? -1 : 1;
             }
 
             return $a->distance < $b->distance ? - 1 : 1;
         } );
 
-        return StationRessource::collection( $sorted );
+
+        foreach ( $sorted as $station ) {
+            echo \App\Facades\AppHelper::removeAccents($station->name) . ' ' . $station->name_fr . ' ' . $station->name_en . ' ' . $station->distance . '<br>';
+        }
+
+        return;
+
+        return StationRessource::collection( $sorted->take( 10 ) );
     }
 
     /**
