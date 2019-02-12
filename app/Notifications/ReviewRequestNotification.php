2e12525bd7e8f13e016090dd00d682ed36a872a8
= <=>
2019-02-12 02:43:54 +0000

    Send email from notification, and send notification from controller.

---
+++ app/Notifications/ReviewRequestNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Mail\ReviewRequestEmail;
+use App\Models\Discussion;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ReviewRequestNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $discussion;
+
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Discussion $discussion)
+    {
+        $this->$discussion = $discussion;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return ReviewRequestEmail;
+     */
+    public function toMail($notifiable)
+    {
+        return new ReviewRequestEmail($this->discussion->buyer, $this->discussion);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        $discussion_id = $this->discussion->id;
+        $ticket_id = $this->discussion->ticket_id;
+        return [
+            'text' => __('email.review_request'),
+            'discussion_id' => $discussion_id,
+            'ticket_id' => $ticket_id,
+            'link' => route('tickets.edit', ['ticket_id' => $ticket_id]),
+            'color'=> 'success'
+        ];
+    }
+}
---
+++ resources/views/emails/fr/review_request.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+
+    # Hello  {{$user->first_name}} !
+
+    Chez PasseTonBillet, nous voulons offrir la meilleure expérience possible à un client et réduire
+    sur les escroqueries et les activités frauduleuses. Cela peut parfois être difficile à réaliser sans expérience de première main.
+    C'est pourquoi nous sollicitons votre aide pour rendre cela possible.
+    Nous sommes ravis que vous ayez récemment vendu un billet et nous aimerions en savoir plus sur votre expérience avec PasseTonBillet.
+    Veuillez cliquer sur le lien ci-dessous pour prendre 5 minutes pour remplir notre formulaire de commentaires et fournir des suggestions ou des domaines que vous pensez pouvoir améliorer.
+
+    @component('mail::button', ['url' => route('home') . '#', 'color'=>'blue'])
+        Votre avis
+    @endcomponent
+
+
+@endcomponent
