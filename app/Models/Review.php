d86c1964f76f7574e4d48cdac6a6645e2d4cda81
= <=>
2019-04-02 09:00:30 +0100

    Added more sample reviews to Review::getReviews() function in Models/Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -56,27 +56,55 @@ class Review extends Model
     public static function getReviews( $n ) {
         $reviews = [
             [
-                'name' =>  'Euginie',
+                'name' =>  'Eugenie',
                 'date' => '3rd March 2019',
                 'rating' => '5',
-                'picture' => secure_asset(''),
-                'text' => ''
+                'picture' => secure_asset('img/ninie.jpg'),
+                'text' => 'A peine inscrite j\'ai déjà vendu mon A/R Paris Londres. Super site!!'
 
             ],
             [
                 'name' => 'Balthazar',
                 'date' => '2nd Febuary 2019',
                 'rating' => '5',
-                'picture' => secure_asset(''),
-                'text' => ''
+                'picture' => secure_asset('img/balou.jpg'),
+                'text' => 'Tout s’est super bien passé, je réutiliserai le site pour sûr !'
 
             ],
             [
                 'name' => 'Kristelle',
                 'date' => '10th March 2019',
                 'rating' => '5',
-                'picture' => secure_asset(''),
-                'text' => ''
+                'picture' => secure_asset('img/kristelle.jpg'),
+                'text' => 'Enfin une plateforme sécurisée, bien mieux que les groupes Facebook !'
+            ],
+            [
+                'name' => 'Christophe',
+                'date' => '20th March 2019',
+                'rating' => '5',
+                'picture' => secure_asset('img/kristelle.jpg'),
+                'text' => 'Genial billet vendu en 30 mn. Systeme tres securise.'
+            ],
+            [
+                'name' => 'Reese',
+                'date' => '4th March 2019',
+                'rating' => '5',
+                'picture' => secure_asset('img/reese.jpg'),
+                'text' => 'Tres bien ! Pouvez vous ajouter un taux de billet vendu sans probleme, et potentiellement un service qui permet d effectuer la transaction par le site.'
+            ],
+            [
+                'name' => 'Cydney',
+                'date' => '9 January 2019',
+                'rating' => '5',
+                'picture' => secure_asset('img/cydney.jpg'),
+                'text' => 'Inscrit il y’a quelque jours, 2 billets revendus ...'
+            ],
+            [
+                'name' => 'Casimer',
+                'date' => '18 January 2018',
+                'rating' => '5',
+                'picture' => secure_asset( 'img/casimer.jpg' ),
+                'text' => 'Très  facile et rapide'
             ]
         ];
 
